2c871cb11f553485d98f91d73d6e978d391ae486
Kodux78k <koduxlive@gmail.com>
2025-11-02 22:17:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hub78k-v1';
+const CACHE = '78khub-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -6,17 +6,10 @@ const ASSETS = [
   './icon-192.png',
   './icon-512.png'
 ];
-
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-});
-
+self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
